e61b1f1986c700a7cc0876534b40899632616287
Roberto Principio Jr <rdprincipio.jr@gmail.com>
2021-01-14 08:01:30 +0800

    added class user properties and constructor

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
-echo 'test';
+
+class User {
+  public $uid, $firstName, $lastName, $email, $phone;
+
+  public function __construct($firstName, $lastName, $email, $phone) {
+    // $this->uid = 'implement later';
+    $this->firstName = $firstName;
+    $this->lastName = $lastName;
+    $this->email = $email;
+    $this->phone = $phone;
+  }
+}
+
+
+$user = new User('Juan', 'dela Cruz', 'juan.dcruz@gmail.com', '1234567890');
+echo var_dump(get_object_vars($user));
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Simple SignUp Form</title>
+</head>
+<body>
+  
+</body>
+</html>
