4853710575740fafacacd86dd0509a22a2ee1839
Roberto Principio Jr <rdprincipio.jr@gmail.com>
2021-01-14 11:21:05 +0800

    added cleaner for input sanitization and validation

---
+++ cleaner.php
@@ -0,0 +1,81 @@
+<?php
+include 'classes/user.php';
+
+class UserCleaner {
+  private $user;
+  private $errors = [];
+
+  public function __construct($data) {
+    $this->user = new User($data['firstName'], $data['lastName'], $data['email'], $data['phone']);
+  }
+
+  public function validate() {
+
+    $requiredFields = ['firstName', 'email'];
+    
+    $r = new ReflectionClass($this->user);
+    foreach($requiredFields as $requiredField) {
+
+      $p = $r->getProperty($requiredField);  
+      $p->setAccessible(true);
+
+      if (empty($p->getValue($this->user))) {
+        $this->AddError($requiredField, "$requiredField is missing");
+        return;
+      }
+    }
+  
+  if (!empty($this->errors)) {
+    return $this->errors;
+  }  
+
+  $this->cleanFirstName();  
+  $this->cleanLastName();  
+  $this->cleanEmail();  
+  $this->cleanPhone();  
+  return $this->errors;
+  
+  }
+
+  private function cleanFirstName() {
+    $clean = filter_var($this->user->getFirstName(), FILTER_SANITIZE_STRING);
+    $this->user->setFirstName($clean);
+  }
+
+  private function cleanLastName() {
+    if (!empty($this->user->getLastName())) { 
+      $clean = filter_var($this->user->getLastName(), FILTER_SANITIZE_STRING);
+      $this->user->setLastName($clean);
+    }
+  }
+
+  private function cleanEmail() {
+    $clean = filter_var($this->user->getEmail(), FILTER_SANITIZE_EMAIL);
+    $check = filter_var($clean, FILTER_VALIDATE_EMAIL);
+
+    if ($check != false) {
+      $this->user->setEmail($clean);
+    } else {
+      $this->AddError('email', 'Email is invalid');
+    }
+  }
+
+  private function cleanPhone() {
+    if (!empty($this->user->getPhone())) {
+      $clean = filter_var($this->user->getPhone(), FILTER_SANITIZE_NUMBER_INT);
+      $check = filter_var($clean, FILTER_VALIDATE_INT);
+
+      if ($check != false) {
+        $this->user->setPhone($clean);
+      } else {
+        $this->AddError('phone', 'Phone number is invalid');
+      }
+    }
+  }
+
+  private function AddError($field, $msg) {
+    $this->errors[$field] = $msg;
+  }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
+require 'cleaner.php';
+
 if (isset($_POST['submit'])) {
-  echo 'submitted';
+  $newUser = new UserCleaner($_POST);
+  $errors = $newUser->validate();
+  echo print_r($errors);
+
+  if (empty($errors)) {
+    echo 'no errors';
+  } else {
+    echo 'there are errors!';
+  }
 }
 
 
@@ -11,7 +21,7 @@ if (isset($_POST['submit'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Simple SignUp Form</title>
+  <title>Simple Sign Up Form</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   
 </head>
